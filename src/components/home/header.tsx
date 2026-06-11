fff11cd9bba5370b6f2b9e1c80176e9005774403
tiago pinchieri <tiagopinchieri2024>
2026-06-11 20:31:54 -0300

    feat: iniciar componente sheet

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 
 export const Header = () => {
   return (
-    <header>
+    <header className="flex justify-between items-center p-3">
       <div>
         <Link href="/" title="Link para página inicial">
           <Image
@@ -14,6 +16,18 @@ export const Header = () => {
             title="logotipo da aplicação"
           />
         </Link>
+      </div>
+
+      {/* mobile */}
+      <div>
+        <Sheet>
+          <SheetTrigger>
+            <Menu size={56} className="" />
+          </SheetTrigger>
+          <SheetContent className="bg-white">
+             
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
