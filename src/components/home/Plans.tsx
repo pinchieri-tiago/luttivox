5ca65e934f1ee37f811e53997af0d46ddf1eddda
tiago pinchieri <tiagopinchieri2024>
2026-06-12 00:19:19 -0300

    fix: colocar key prop na div

--- src/components/home/Plans.tsx
+++ src/components/home/Plans.tsx
@@ -44,7 +44,10 @@ export const Plans = () => {
       </div>
       <div className="mt-4 flex flex-col items-center justify-center gap-4 md:flex-row lg:mt-8">
         {cardItems.map((item) => (
-          <div className="min-h-42 min-w-76 rounded-2xl border border-gray-700 p-4 shadow-2xl">
+          <div
+            key={item.title}
+            className="min-h-42 min-w-76 rounded-2xl border border-gray-700 p-4 shadow-2xl"
+          >
             <div className="mb-3 flex items-center">
               <Image
                 src={item.image}
@@ -56,7 +59,7 @@ export const Plans = () => {
               <p className="font-bold">{item.title}</p>
             </div>
             {item.features.map((feat) => (
-              <div className="flex flex-col p-1">
+              <div key={feat.description} className="flex flex-col p-1">
                 <p className="flex max-w-93 items-center gap-2 text-sm">
                   <Image
                     src={feat.image}
@@ -69,9 +72,9 @@ export const Plans = () => {
                 </p>
               </div>
             ))}
-            <div className="mt-3 w-full  ">
+            <div className="mt-3 w-full">
               <Link
-                className="from-primary-700 font-bold flex justify-center w-full hover:from-secondary-600 hover:to-primary-700 to-secondary-600 rounded-xl bg-linear-to-r px-4 py-2 text-center text-lg text-white transition-all duration-500 md:text-lg"
+                className="from-primary-700 hover:from-secondary-600 hover:to-primary-700 to-secondary-600 flex w-full justify-center rounded-xl bg-linear-to-r px-4 py-2 text-center text-lg font-bold text-white transition-all duration-500 md:text-lg"
                 href={item.button === "gratuito" ? "/registrar" : "/planos"}
               >
                 {item.button === "gratuito" ? "Começar" : "Assinar"}
