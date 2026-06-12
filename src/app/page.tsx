e10571d7521b9082ef0dc8b3edf50f15c6105917
tiago pinchieri <tiagopinchieri2024>
2026-06-11 23:13:03 -0300

    feat: iniciar componente de planos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Benefits } from "@/components/home/benefits";
 import { Demo } from "@/components/home/demo";
 import { Header } from "@/components/home/header";
 import { Hero } from "@/components/home/hero";
+import { Plans } from "@/components/home/Plans";
 
 export default function HomePage() {
   return (
@@ -12,6 +13,7 @@ export default function HomePage() {
         <Benefits />
         <About />
         <Demo  />
+        <Plans />
      </div>
   );
 }
