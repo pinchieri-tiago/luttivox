f6c423f0fcd313af3ae099f864225affe57d5055
tiago pinchieri <tiagopinchieri2024>
2026-06-13 20:56:41 -0300

    feat: inserir imagem em layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,25 @@
+import Image from "next/image";
 
 interface LayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export default function Layout({children}: LayoutProps){
-    return (
-        <div>
-            <div>
-
-            </div>
-            <div>
-                
-            </div>
-        </div>
-    );
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="bg-secondary-50 min-h-screen">
+      <div>
+        <Image
+          src="/neural.png"
+          alt="imagem de representação de inteligência artificial"
+          width={400}
+          height={400}
+          loading="eager"
+          className="h-auto w-45"
+          priority
+          quality={100} 
+        />
+      </div>
+      <div>{children}</div>
+    </div>
+  );
 }
