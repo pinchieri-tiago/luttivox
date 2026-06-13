983d0e282cbe541e17164e20cfb61a5335f7028f
tiago pinchieri <tiagopinchieri2024>
2026-06-13 20:51:07 -0300

    feat: iniciar implementação de layout de autenticação

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+
+interface LayoutProps {
+    children: React.ReactNode
+}
+
+export default function Layout({children}: LayoutProps){
+    return (
+        <div>
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
