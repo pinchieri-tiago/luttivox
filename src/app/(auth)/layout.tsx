c082cd6a2fd5364f5ba691d7dbefa34b02a12703
tiago pinchieri <tiagopinchieri2024>
2026-06-13 22:02:56 -0300

    feat: criar fronend de login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -6,20 +7,26 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="bg-secondary-50 min-h-screen">
-      <div>
-        <Image
-          src="/neural.png"
-          alt="imagem de representação de inteligência artificial"
-          width={400}
-          height={400}
-          loading="eager"
-          className="h-auto w-45"
-          priority
-          quality={100} 
-        />
+    <div className="bg-secondary-50 flex min-h-screen flex-col items-center justify-center">
+      <div className="my-4 flex min-w-76 flex-col items-center gap-4 rounded-4xl bg-white p-5 md:min-w-2xl md:flex-row md:justify-center lg:min-w-4xl">
+        <div className="order-2">
+          <Link href="/" className="flex justify-center">
+            <Image
+              src="/neural.png"
+              alt="imagem de representação de inteligência artificial"
+              width={400}
+              height={400}
+              loading="eager"
+              className="h-auto w-45 md:w-77"
+              priority
+              quality={100}
+            />
+          </Link>
+        </div>
+        <div className="order-1 w-full md:max-w-66 lg:max-w-105">
+          {children}
+        </div>
       </div>
-      <div>{children}</div>
     </div>
   );
 }
--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,11 +1,121 @@
 "use client";
 
+import { emailSchema } from "@/validation/email-schema";
+import { passwordSchema } from "@/validation/password-schema";
+import z from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import Link from "next/link";
 
+const loginSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+});
 
 export const Login = () => {
-  return <form>
-       <div>
-          
-       </div>
-  </form>;
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const submit = async (data: z.infer<typeof loginSchema>) => {
+     
+  };
+  return (
+    <div className="w-full">
+      <div className="my-3 flex flex-col items-center justify-center">
+        <div>
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="logotipo"
+              width={200}
+              height={200}
+              className="w-44"
+              priority
+              loading="eager"
+            />
+          </Link>
+        </div>
+        <h2 className="font-bold">Entre com suas crêdenciais</h2>
+      </div>
+      <form className="w-full" onSubmit={form.handleSubmit(submit)}>
+        <fieldset className="flex flex-col gap-3.5">
+          <div className="w-full">
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <div>
+                  <label htmlFor="email" className="text-primary-700 font-bold">
+                    Email
+                  </label>
+                  <input
+                    {...field}
+                    type="email"
+                    placeholder="Digite seu email"
+                    className="bg-secondary-50 text-primary-700 placeholder:text-primay-300 w-full rounded-3xl p-3 placeholder:font-bold"
+                  />
+                </div>
+              )}
+            />
+            {form.formState.errors?.email && (
+              <p className="mb-3 text-red-500">
+                {form.formState.errors?.email?.message}
+              </p>
+            )}
+          </div>
+          <div className="w-full">
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="text-primary-700 font-bold"
+                  >
+                    Senha
+                  </label>
+                  <input
+                    {...field}
+                    type="password"
+                    placeholder="*************"
+                    className="bg-secondary-50 text-primary-700 placeholder:text-primay-300 w-full rounded-3xl p-3 placeholder:font-bold"
+                  />
+                </div>
+              )}
+            />
+            {form.formState.errors?.password && (
+              <p className="mb-3 text-red-500">
+                {form.formState.errors?.password?.message}
+              </p>
+            )}
+          </div>
+          <div className="my-5">
+            <button
+              disabled={form.formState.isSubmitting}
+              className="from-primary-700 cursor-pointer hover:from-secondary-600 hover:to-primary-700 to-secondary-600 flex w-full justify-center rounded-xl bg-linear-to-r px-4 py-2 text-center text-lg font-bold text-white transition-all duration-500 disabled:opacity-40 md:text-lg"
+            >
+              {form.formState.isSubmitting ? (
+                <div className="size-8 animate-spin rounded-full border-4 border-white border-t-transparent" />
+              ) : (
+                "Entrar"
+              )}
+            </button>
+          </div>
+        </fieldset>
+      </form>
+      <div className="mb-4 font-bold">
+        Não tem conta?{" "}
+        <Link href="/registrar" className="text-primary-500 underline">
+          Registre-se
+        </Link>
+      </div>
+    </div>
+  );
 };
